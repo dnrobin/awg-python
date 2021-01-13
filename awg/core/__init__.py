2593bd320fbaf19037a65487ca6c86f7383074df
David Turgeon <turgeon.david@hotmail.com>
2021-01-13 16:12:10 -0500

    The wgindex function is done

--- awg/core/__init__.py
+++ awg/core/__init__.py
@@ -243,17 +243,108 @@ def slabmode(lmbda0,t,na,nc,ns,**kwargs):
 	return E,H,y,neff
 
 
-x = slabmode(1.5,1,SiO2,Si,SiO2, points = 10)
-print(x)
-
 #a = [10,9,8,7,6,5,4,3,2,1,0]
 #for i, e in enumerate(a):
 #	print(i,e)
 
 
 
-def wgindex():
-    pass
+def wgindex(lmbda0,w,h,t,na,nc,ns,**kwargs):
+	"""Effective index method for guided modes in arbitrary waveguide
+	
+	 DESCRIPTION:
+	   solves for the TE (or TM) effective index of an etched waveguide
+	   structure using the effectice index method.
+	
+	 USAGE:
+	   - get effective index for supported TE-like modes:
+	   neff = eim_index(1.55, 0.5, 0.22, 0.09, 1, 3.47, 1.44)
+	
+	              |<   w   >|
+	               _________           _____
+	              |         |            ^
+	  ___    _____|         |_____ 
+	   ^                                 h
+	   t                                  
+	  _v_    _____________________     __v__
+	
+	          II  |    I    |  II
+	
+	 INPUT:
+	 lambda0   - free-space wavelength
+	 w         - core width
+	 h         - slab thickness
+	 t         - slab thickness
+	               t < h  : rib waveguide
+	               t == 0 : rectangular waveguide w x h
+	               t == h : uniform slab of thickness t
+	 na        - (top) oxide cladding layer material index
+	 nc        - (middle) core layer material index
+	 ns        - (bottom) substrate layer material index
+	
+	 OPTIONS:
+	 Modes - number of modes to solve
+	 Polarisation - one of 'TE' or 'TM'
+	
+	 OUTPUT:
+	 neff - TE (or TM) mode index (array of index if multimode)
+	
+	 NOTE: it is possible to provide a function of the form n = material(lambda0) for 
+	 the refraction index which will be called using lambda0. """
+
+
+	_in = kwargs
+	if "Modes" not in _in.keys():
+		_in["Modes"] = np.inf
+
+	if "Polarisation" in _in.keys():
+		if _in["Polarisation"] in ["TE","te","TM","tm"]:
+			pass
+	else:
+		_in["Polarisation"] = "TE"
+
+	if (type(na) == types.FunctionType) or (str(type(na)) == "<class 'material.Material.Material'>"):
+		na = na(lmbda0)
+	if (type(nc) == types.FunctionType) or (str(type(nc)) == "<class 'material.Material.Material'>"):
+		nc = nc(lmbda0)
+	if (type(ns) == types.FunctionType) or (str(type(ns)) == "<class 'material.Material.Material'>"):
+		ns = ns(lmbda0)
+
+	t = clamp(t,0,h)
+
+	neff_I = slabindex(lmbda0,h,na,nc,ns,Modes = _in["Modes"], Polarisation = _in["Polarisation"])
+
+	if t == h:
+		neff = neff_I
+		return neff
+
+	if t > 0:
+		neff_II = slabindex(lmbda0,t,na,nc,ns,Modes = _in["Modes"],Polarisation = _in["Polarisation"])
+	else:
+		neff_II = na
+	
+	neff = []
+
+	if _in["Polarisation"].upper() in "TE":
+
+		"""if type(neff_I) == float:
+			neff_I = [neff_I]
+		if type(neff_II) == float:
+			neff_II = [neff_II]"""
+
+		for m in range(min(len(neff_I),len(neff_II))):
+			n = slabindex(lmbda0,w,neff_II[m],neff_I[m],neff_II[m],Modes = _in["Modes"],Polarisation = "TM")
+			for i in n:
+				if i > max(ns,na):
+					neff.append(i)
+	else:
+		for m in range(min(len(neff_I),len(neff_II))):
+			n = slabindex(lmbda0,w,neff_II[m],neff_I[m],neff_II[m],Modes = _in["Modes"],Polarisation = "TE")
+			for i in n:
+				if i > max(ns,na):
+					neff.append(i)
+	return neff
+
 
 def wgmode():
     pass
